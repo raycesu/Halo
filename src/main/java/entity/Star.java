667e1a959604316cf9904065a17c93993d6f6ce0
Bryan Kim <bryankim@macbookair.home>
2026-07-22 23:38:56 -0400

    Updated Entity List

--- src/main/java/entity/Star.java
+++ src/main/java/entity/Star.java
@@ -1,4 +1,36 @@
 package entity;
 
 public class Star {
+    private String name;
+    private String ra;
+    private String dec;
+    private String brightness;
+
+    public Star(String name, String ra, String dec, String brightness) {
+        this.name = name;
+        this.ra = ra;
+        this.dec = dec;
+        this.brightness = brightness;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getRa() {
+        return ra;
+    }
+    public void setRa(String ra) {
+        this.ra = ra;
+    }
+    public String getDec() {
+        return dec;
+    }
+    public void setDec(String dec) {
+        this.dec = dec;
+    }
+
 }
