b49f1b78388ae5ee3f353d6f82aecb9351c61fb3
Rachel <lokyi.yeung@mail.utoronto.ca>
2026-07-26 21:05:05 -0400

    Add filter and visualization

---
+++ src/main/java/view/VisibilityFilter.java
@@ -0,0 +1,26 @@
+package view;
+
+import java.util.ArrayList;
+import java.util.List;
+import entity.Star;
+
+public class VisibilityFilter {
+    private static final double MIN_VISIBLE_ALTITUDE = 0.0; // confirm altitude
+
+    public static List<Star> filterVisible(List<Star> allstars) {
+        List<Star> visible =  new ArrayList<>();
+        for (Star star : allstars) {
+            double altitude = star.getAltitude();
+            if (Double.isNaN(altitude)) {
+                continue;
+            }
+            if (altitude < MIN_VISIBLE_ALTITUDE) {
+                continue;
+            }
+            visible.add(star);
+        }
+        return visible;
+    }
+
+
+}
