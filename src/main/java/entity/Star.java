3627b1ae64b2f42ab8c65e8320a8ac47c5a742a4
Deniz-Coban <deniz12042006@gmail.com>
2026-07-28 19:18:40 -0400

    Implemented entities and small fixes

--- src/main/java/entity/Star.java
+++ src/main/java/entity/Star.java
@@ -1,42 +1,95 @@
 package entity;
 
-public class Star {
-    private String name;
-    private Double ra;
-    private Double dec;
-    private String brightness;
+public final class Star {
 
-    public Star(String name, Double ra, Double dec, String brightness) {
-        this.name = name;
-        this.ra = ra;
-        this.dec = dec;
-        this.brightness = brightness;
-    }
+    private final String catalogueId;
+    private final String displayName;
+    private final double rightAscension;
+    private final double declination;
+    private final double apparentMagnitude;
+    private final String constellationRegion;
+    private final String spectralType;
+    private final String description;
 
+    private double altitude = Double.NaN;
+    private double azimuth = Double.NaN;
 
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public Double getRa() {
-        return ra;
-    }
-    public void setRa(Double ra) {
-        this.ra = ra;
-    }
-    public Double getDec() {
-        return dec;
-    }
-    public void setDec(Double dec) {
-        this.dec = dec;
-    }
-    public String getBrightness() {
-        return brightness;
-    }
-    public void setBrightness(String brightness) {
-        this.brightness = brightness;
+    public Star(
+            final String catalogueId,
+            final String displayName,
+            final double rightAscension,
+            final double declination,
+            final double apparentMagnitude,
+            final String constellationRegion,
+            final String spectralType,
+            final String description) {
+
+        this.catalogueId = catalogueId;
+        this.displayName = displayName;
+        this.rightAscension = rightAscension;
+        this.declination = declination;
+        this.apparentMagnitude = apparentMagnitude;
+        this.constellationRegion = constellationRegion;
+        this.spectralType = spectralType;
+        this.description = description;
     }
 
+    public String getCatalogueId() {
+        return catalogueId;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public double getRightAscension() {
+        return rightAscension;
+    }
+
+    public double getDeclination() {
+        return declination;
+    }
+
+    public double getApparentMagnitude() {
+        return apparentMagnitude;
+    }
+
+    public String getConstellationRegion() {
+        return constellationRegion;
+    }
+
+    public String getSpectralType() {
+        return spectralType;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getAltitude() {
+        return altitude;
+    }
+
+    public double getAzimuth() {
+        return azimuth;
+    }
+
+    public void updateHorizontalPosition(
+            final double altitude,
+            final double azimuth) {
+        this.altitude = altitude;
+        this.azimuth = azimuth;
+    }
+
+    public Star copyForObservation() {
+        return new Star(
+                catalogueId,
+                displayName,
+                rightAscension,
+                declination,
+                apparentMagnitude,
+                constellationRegion,
+                spectralType,
+                description);
+    }
 }
